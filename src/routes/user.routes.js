7d6197aa4baa2bef7f292773f1fc2bf3e32dd680
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-03-08 09:17:39 +0530

    Added all the routes for users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,24 +1,46 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCover,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        { name: "avatar", maxCount: 1},
-        { name: "coverImage", maxCount: 1}
-    ]),
-    registerUser
-    )
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)  
+router.route("/login").post(loginUser);
 
 // Secured Routes
 
-router.route("/logout").post(verifyJWT ,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("/avatar"), updateUserAvatar);
+router
+  .route("/update-cover")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCover);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //getting details form params
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
-router.route("/refresh-token").post(refreshAccessToken)
-
-export default router
+export default router;
